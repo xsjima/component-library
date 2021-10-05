c71d13ebf2dbfe1647f36a29c0ea00d46622d9da
Denis <zabavnikov.com@gmail.com>
2021-10-05 15:59:22 +0700

    add mixin for input's

---
+++ mixins/input.js
@@ -0,0 +1,53 @@
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    id: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    minlength: {
+      type: [Number, String],
+      default: 0
+    },
+    maxlength: {
+      type: [Number, String],
+      default: 0
+    },
+
+    // type number
+    step: [Number, String],
+    min:  [Number, String],
+    max:  [Number, String],
+
+    hasError: Boolean,
+    rounded: Boolean,
+  },
+  computed: {
+    classList() {
+      const classList = [];
+
+      if (this.hasError === true) {
+        classList.push('is-danger');
+      }
+
+      if (this.rounded === true) {
+        classList.push('input--rounded');
+      }
+
+      return classList;
+    },
+    getMinLength() {
+      return parseInt(this.minlength) > 0 ? parseInt(this.minlength) : false
+    },
+    getMaxLength() {
+      return parseInt(this.maxlength) > 0 ? parseInt(this.maxlength) : false
+    }
+  },
+}
