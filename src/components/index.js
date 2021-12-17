494774a8b673ffc207eed29fa7d170418ece173a
Denis <zabavnikov.com@gmail.com>
2021-12-17 17:37:30 +0700

    update

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,24 @@
 /* eslint-disable import/prefer-default-export */
-export { default as VAlert } from './VAlert';
-export { default as VAutocomplete } from './VAutocomplete';
-export { default as VButton } from './VButton';
-export { default as VCheckbox } from './VCheckbox';
-export { default as VChip } from './VChip';
-export { default as VDialog } from './VDialog';
-export { default as VIcon } from './VIcon';
-export { default as VInput } from './VInput';
-export { default as VLike } from './VLike';
-export { default as VTextarea } from './VTextarea';
+import VAlert from './VAlert';
+import VAutocomplete from './VAutocomplete';
+import VButton from './VButton';
+import VCheckbox from './VCheckbox';
+import VChip from './VChip';
+import VDialog from './VDialog';
+import VIcon from './VIcon';
+import VInput from './VInput';
+import VLike from './VLike';
+import VTextarea from './VTextarea';
+
+export {
+  VAlert,
+  VAutocomplete,
+  VButton,
+  VCheckbox,
+  VChip,
+  VDialog,
+  VIcon,
+  VInput,
+  VLike,
+  VTextarea,
+}
