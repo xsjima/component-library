f1480ee75bd7d70fed1ab14a0f6047fca24c288d
Denis <zabavnikov.com@gmail.com>
2022-02-03 15:26:44 +0700

    VSheet update

--- src/components/VSheet/Component.vue
+++ src/components/VSheet/Component.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white rounded-b-lg p-6">
+  <div class="bg-white rounded-t-lg p-6">
     <div class="flex items-center justify-between mb-6">
       <div class="text-lg">{{ title }}</div>
       <v-icon name="x" class="w-6 h-6 text-gray hover:text-black cursor-pointer" @click="$overlay.hide()"></v-icon>
